cb4caa2d7b6de657190ba91f6e87981f0f0ee1d3
Алымбаев Т <timka-555player@mail.ru>
2017-04-24 22:23:47 +0700

    Sam algoritm game

--- hangman.cpp
+++ hangman.cpp
@@ -21,6 +21,56 @@ int main()
     string soFar(THE_WORD.size(), '-'); //часть слова, открытая на данный момент
     string used = ""; //уже отгаданные буквы
     
+      //основа
+    
+    while((wrong<MAX_WRONG) && (soFar != THE_WORD))
+    {
+      cout << "\n\nУ Вас есть всего "<<(MAX_WRONG - wrong);
+      cout << " попыток.\n";
+      cout << "\nВы использовали эти буквы:\n" <<used <<endl;
+      cout << "\nТут слово которое я загадал:\n" << soFar<< endl;
+      
+      char guess;
+      cout << "\n\nВаша буква: ";
+      cin>>guess;
+      guess = toupper(guess); //перевод в верхний регистр
+      // так как загаданное слово записанно в верхнем регистре
+      while (used. find(guess) != string::npos)
+      {
+            cout<<"\nТы уже загадывал "<<guess<<endl;
+            cout<<"Введи букву еще раз: ";
+            cin>>guess;
+            guess=toupper(guess);
+            }
+      used+=guess;
+      if(THE_WORD.find(guess) !=string::npos)
+      {
+            cout<<"Ты прав! "<<guess<<" в слове есть.\n";
+            //обновить переменную soFar, включив в нее новую угаданную букву
+            for(int i=0; i<THE_WORD.length(); ++i)
+            {
+                    if(THE_WORD[i]==guess)
+                    {
+                                          soFar[i]=guess;
+                                          }
+                                          }
+                                          }
+                    else
+                    {
+                        cout<<"Извени. "<<guess<<" в слове нет.\n";
+                        ++wrong;
+                        }
+                        }
+      //конец игры
+      if(wrong==MAX_WRONG)
+      {
+      cout<<"\nТебя повесили!";
+      }
+      else
+      {
+          cout<<"\nТы победил!";
+          }
+      cout<<"\nЗагаданное слово: " <<THE_WORD<<endl;
     
       system("PAUSE");
       }
