fe72fe41cf1de7322ad0cdce8ef0e7e9629826b0
Roman Likhachev <romkin63@gmail.com>
2017-05-17 23:58:29 +0700

    add first tests

---
+++ test/category.cpp
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "ctest.h"
+
+
+CTEST(Speed, testspeed) {
+    usleep(2000);
+}
+
+
+
---
+++ test/hangman.cpp
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "ctest.h"
+#include "hangman.h"
+
+
+CTEST(Speedtest,Validtest) {
+    usleep(2000);
+}
+
+CTEST(test1, NULL) {
+    const int ;
+    int p=0;  
+     
+                        
+    ASSERT_EQUAL(1, p);
+} 
+
+CTEST(wordtest,imputtest) {
+    
+    const string buk="1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char guess ='A';                       
+
+    ASSERT_EQUAL(guess,buk);
+}
+
+CTEST(categtest, checkcateg) {
+    
+    const int q=1;
+    const  ;                       
+
+    ASSERT_EQUAL(9000, sum1);
+}
+
+CTEST(filetest, openslovotext) {
+
+	const int p=0;
+	const int a=1;
+    
+    if ( (f = fopen("slovo.txt", "r")) == NULL )
+     {
+	
+	 p=1;
+
+	 }                      
+
+    ASSERT_EQUAL(a,p);
+}
+
+
+CTEST(categtest, checkcateg) {
+    
+    const int q=3;
+    const int ;                       
+
+
+if(wrong!=MAX_WRONG)
+{
+      THE_WORD;
+      }
+
+ ASSERT_EQUAL(9000, sum1);
+}
